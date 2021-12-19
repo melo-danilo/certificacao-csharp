dd2c80b2499a7b33426b142292331532124ca5f0
Danilo Melo <54605570+melo-danilo@users.noreply.github.com>
2021-12-19 13:25:33 -0300

    Create TiposDeValor.cs

---
+++ TiposDeValor.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace certificacao_csharp_roteiro
+{
+    class TiposDeValor : IAulaItem
+    {
+        public void Executar()
+        {
+            int idade;
+            idade = 28;
+            Console.WriteLine(idade);
+
+            int copiaIdade = idade;
+            Console.WriteLine($"idade: {idade}");
+            Console.WriteLine($"copiaIdade: {copiaIdade}");
+
+            idade = 23;
+            Console.WriteLine($"idade: {idade}");
+            Console.WriteLine($"copiaIdade: {copiaIdade}");
+
+            int? idade2 = null;
+            System.Nullable<int> idade3 = null;
+        }
+    }
+}
